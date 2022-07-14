5690558943db8543cd10f9e5ec32eed36e8748ea
vivek0623 <vivekbangera44@gmail.com>
2022-07-14 15:49:20 +0530

    committed

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 6; ++i)
+        {
+            for (int j = 1; j <= i; ++j)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
